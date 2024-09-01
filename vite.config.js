edddb44c1873088e30872b116aaff3f712b9e31e
sgrs rgrdgd <skpsleong@gmail.com>
2024-09-02 02:15:16 +0800

    Resolved merge conflicts

--- vite.config.js
+++ vite.config.js
@@ -16,4 +16,13 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
   },
+  server: {
+    proxy: {
+      '/api': {
+        target: 'https://personal-use-1857a.firebaseapp.com',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, '')
+      }
+    }
+  }
 })
