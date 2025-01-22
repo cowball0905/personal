5562703bf32cc396ed5e15f976545526153edc6a
Ken Wong <51465874+cowball0905@users.noreply.github.com>
2025-01-22 16:24:54 +0800

    Update source code for new version

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import VueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/vue-personal-page/',
+  base: '/personal/',
   plugins: [
     vue(),
     VueDevTools(),
